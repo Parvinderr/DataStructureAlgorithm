ece05c099e25ce77250348a44bd492a3e8ba2bbe
Parvinderr <69068497+Parvinderr@users.noreply.github.com>
2021-02-18 11:51:16 +0530

    Create practice8.java

---
+++ Practice/practice8.java
@@ -0,0 +1,18 @@
+// Reverse a number
+
+import java.util.*;
+
+public class Main{
+   
+   public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int num = scn.nextInt();
+   
+    while (num!=0){
+        int digit = num%10;
+        System.out.println(digit);
+        num = num/10;
+        
+    }
+    }
+}
