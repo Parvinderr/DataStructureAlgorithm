4666f9c95da2bb699806bb56e0fd39d04f27d643
Parvinderr <69068497+Parvinderr@users.noreply.github.com>
2021-02-20 12:07:25 +0530

    Create example3.java

---
+++ Array/example3.java
@@ -0,0 +1,39 @@
+// Reverse Of An Array
+
+
+
+public class Main{
+  public static void display(int[] a){
+    StringBuilder sb = new StringBuilder();
+
+    for(int val: a){
+      sb.append(val + " ");
+    }
+    System.out.println(sb);
+  }
+
+  public static void reverse(int[] a){
+    int l = a.length;
+    int temp=0;
+    for (int i =0;i<l/2;i++){
+        temp = a[i];
+        a[i]= a[l-1-i];
+        a[l-1-i]=temp;
+       
+    }
+  }
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+
+    reverse(a);
+    display(a);
+ }
+
+}
