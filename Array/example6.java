698bd093fdd3d342abfb740dbc3a142818326bca
Parvinderr <69068497+Parvinderr@users.noreply.github.com>
2021-02-23 12:17:19 +0530

    Create example6.java

---
+++ Array/example6.java
@@ -0,0 +1,39 @@
+// Find element in unknown Rotated Array in O(logn)
+
+public class Main{
+  
+  public static void main(String[] args) {
+     // Find element in unknown rotated array
+      int[] arr = {3,4,5,6,7,8,9,1,2};
+      int e =4;
+     findElement(arr,e);
+     }
+  public static void findElement(int[] arr,int e){
+    int first = arr[0];
+    int last = arr[arr.length-1];
+    int length = arr.length/2;
+
+
+    if(e<=arr[length] && e>=first){
+    for(int i =0;i<arr.length;i++){
+      int res = arr[i];
+      if(res ==e){
+        System.out.println("Found at index: "+i);
+        break;
+      }
+    }
+    }
+    else if(e<first || e>arr[length]){
+      for(int i =0;i<arr.length;i++){
+      int res = arr[i];
+      if(res ==e){
+        System.out.println("Found at index: "+i);
+        break;
+      }
+      }
+      }else{
+      System.out.println("Not Found");
+    }
+
+    }
+    }
