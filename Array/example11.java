83626444ea37250889eeb55c04abd234924f70dd
Parvinderr <69068497+Parvinderr@users.noreply.github.com>
2021-02-28 11:34:44 +0530

    Create example11.java

---
+++ Array/example11.java
@@ -0,0 +1,41 @@
+// Remove Prime Numeber From ArrayList
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void solution(ArrayList<Integer> al){
+		for(int i =0;i<al.size();i++){
+		    if(isPrime(al.get(i))==true){
+		        al.remove(i);
+		    }
+		}
+	}
+	public static boolean isPrime(int x){
+	    int count =0;
+	    for(int i =2;i*i<=x;i++){
+	        if(x%i==0){
+	           count++;
+	           break;
+	        }
+	    }
+	    if(count >0){
+	        return false;
+	    }
+	    return true;
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		ArrayList<Integer> al = new ArrayList<>();
+		for(int i = 0 ; i < n; i++){
+			al.add(scn.nextInt());
+		}
+		solution(al);
+		System.out.println(al);
+	}
+
+}
